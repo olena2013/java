ef23f27d1724614a78141d8b7a1b9199c60452f8
olena2013 <ltunik@mail.ru>
2017-11-14 13:55:20 -0800

    Создание массивов и коллекций

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -29,11 +30,11 @@ public class ApplicationManager {
 
     public void init() {
 
-        if(browser == BrowserType.FIREFOX){
+        if(Objects.equals(browser, BrowserType.FIREFOX)){
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if(browser == BrowserType.CHROME) {
+        } else if(Objects.equals(browser, BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE)   {
+        } else if (Objects.equals(browser, BrowserType.IE))   {
             wd = new InternetExplorerDriver();
         }
 
