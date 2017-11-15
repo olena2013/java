9e0fc9ee524f921ac2e9251d13d17631a5b11d19
olena2013 <ltunik@mail.ru>
2017-11-14 19:58:35 -0800

    Создание Лямбда-выражения

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,4 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -10,24 +11,17 @@ import java.util.List;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testGroupCreation () {
+    public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        List<GroupData>before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test2", "test3", "tes4");
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("test3", "test4", "tes5");
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() +1);
+        Assert.assertEquals(after.size(), before.size() + 1);
 
-
-        int max =0;
-        for (GroupData g: after){
-            if(g.getId()> max){
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
