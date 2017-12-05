51159556cc7a70144afcc40698db0c25c03a9258
olena2013 <ltunik@mail.ru>
2017-12-05 14:42:45 -0800

    Реализовали проверки списков, загружаемых из базы данных , для модификации групп

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -27,6 +27,7 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper ;
     private GroupHelper groupHelper;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -47,6 +48,9 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+        dbHelper = new DbHelper();
+
         if(Objects.equals(browser, BrowserType.FIREFOX)){
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         } else if(Objects.equals(browser, BrowserType.CHROME)) {
@@ -62,6 +66,7 @@ public class ApplicationManager {
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
         sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
 
     }
 
@@ -88,6 +93,8 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
+
+    public DbHelper db(){return dbHelper; }
 
 
 }
